a854c08e0e6ae0bad039eb74890a4d40aa3b026b
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-04-04 11:41:35 +0100

    Some region, variant and gene web services implemented

--- eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/VariantWSServer.java
+++ eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/VariantWSServer.java
@@ -1,33 +1,37 @@
 package uk.ac.ebi.variation.eva.server.ws;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.io.IOException;
-import org.opencb.biodata.models.feature.Region;
 import org.opencb.opencga.lib.auth.IllegalOpenCGACredentialsException;
 import org.opencb.opencga.lib.auth.MongoCredentials;
 import org.opencb.opencga.storage.variant.VariantDBAdaptor;
 import org.opencb.opencga.storage.variant.mongodb.VariantMongoDBAdaptor;
 
 /**
- * Created by imedina on 01/04/14.
+ *
+ * @author Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
  */
-@Path("/{version}/segment/{region}")
+@Path("/{version}/variants/{variantId}")
 @Produces(MediaType.APPLICATION_JSON)
-public class RegionWSServer extends EvaWSServer {
+public class VariantWSServer extends EvaWSServer {
 
     private VariantDBAdaptor variantMongoQueryBuilder;
     private MongoCredentials credentials;
 
-    public RegionWSServer() {
+    public VariantWSServer() {
 
     }
 
-    public RegionWSServer(@DefaultValue("") @PathParam("version")String version, @Context UriInfo uriInfo, @Context HttpServletRequest hsr) throws IOException {
+    public VariantWSServer(@DefaultValue("") @PathParam("version")String version, @Context UriInfo uriInfo, @Context HttpServletRequest hsr) throws IOException {
         super(version, uriInfo, hsr);
         try {
             credentials = new MongoCredentials("mongos-hxvm-dev-001", 27017, "eva_hsapiens", "biouser", "biopass");
@@ -38,9 +42,9 @@ public class RegionWSServer extends EvaWSServer {
     }
 
     @GET
-    @Path("/variants")
-    public Response getVariantsByRegion(@PathParam("region") String regionId) {
-        Region region = Region.parseRegion(regionId);
-        return createOkResponse(variantMongoQueryBuilder.getAllVariantsByRegion(region, queryOptions));
+    @Path("/info")
+    public Response getVariantById(@PathParam("variantId") String variantId) {
+        return createOkResponse(variantMongoQueryBuilder.getVariantById(variantId, queryOptions));
     }
+    
 }
