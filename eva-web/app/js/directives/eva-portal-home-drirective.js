decd5fbde7681e2c82063dac971d6727f1563a7a
kandaj <kandaj@ebi.ac.uk>
2014-04-24 11:54:03 +0100

    home page content added

---
+++ eva-web/app/js/directives/eva-portal-home-drirective.js
@@ -0,0 +1,15 @@
+angular.module('homeWidgetModule', []).directive('homeWidget', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        transclude: true,
+        templateUrl: 'views/home.html',
+        link: function($scope, element, attr) {
+            $scope.hometest = 'bla bla';
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+
+
+            }
+    }
+});
