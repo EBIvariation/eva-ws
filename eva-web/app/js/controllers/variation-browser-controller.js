8c008c5af8e5b74390f2ad5ee4c06d76b60a9b75
kandaj <kandaj@ebi.ac.uk>
2014-04-24 16:34:24 +0100

    codes cleaned

--- eva-web/app/js/controllers/variation-browser-controller.js
+++ eva-web/app/js/controllers/variation-browser-controller.js
@@ -89,31 +89,6 @@ var variationCtrl = evaApp.controller('variationBrowserCtrl', ['$scope', '$rootS
 
     $scope.gene = 'BRCA1';
 
-    $scope.message = ebiAppDomainHostService.message;
-
-    $scope.infoColumns  = {
-        AF: 'af',
-        AC: 'ac',
-        VF: 'vf',
-        AA: 'aa',
-        AN: 'an'
-    };
-
-    $scope.infoColumnsFilter = [];
-
-    $scope.colors = [
-        {name:'black', shade:'dark', state:false},
-        {name:'white', shade:'light',state:false},
-        {name:'red', shade:'dark',state:false},
-        {name:'blue', shade:'dark',state:false},
-        {name:'yellow', shade:'light',state:false}
-    ];
-    $scope.color = $scope.colors[2];
-
-    $scope.data = ebiAppDomainHostService.data;
-
-
-
     $scope.barChart = {
         options: {
             chart: {
@@ -146,90 +121,6 @@ var variationCtrl = evaApp.controller('variationBrowserCtrl', ['$scope', '$rootS
             enabled: false
         }
     }
-
-    $scope.infoColumnBtnClick = function(infoColumnId){
-
-        this.color.state = !this.color.state;
-
-        var pos = $scope.infoColumnsFilter.indexOf(infoColumnId);
-
-        if (pos == -1) {
-            $scope.infoColumnsFilter.push(infoColumnId);
-        }
-        else {
-            $scope.infoColumnsFilter.splice(pos, 1);
-        }
-
-        if($('#'+infoColumnId).hasClass("btn-primary")){
-
-            $('#'+infoColumnId).removeClass("btn-primary");
-        }
-        else{
-            $('#'+infoColumnId).addClass("btn-primary");
-        }
-
-    }
-
-    $scope.selectAllInfoColumn = function(){
-
-        $('#infoColumnMultiSelect').children().addClass("btn-primary");
-
-        for (var i in $scope.infoColumns) {
-
-            var pos = $scope.infoColumnsFilter.indexOf($scope.infoColumns[i]);
-
-            if (pos == -1) {
-                $scope.infoColumnsFilter.push($scope.infoColumns[i]);
-            }
-        }
-
-    }
-    $scope.deselectAllInfoColumn = function(){
-        $('#infoColumnMultiSelect').children().removeClass("btn-primary");
-        $scope.infoColumnsFilter=[];
-    }
-
-
-
-
-
-
-
-
-
-
-
-    //<!---VariantsEffect Table--->
-    $scope.variantEffectTable = {
-        data: 'variantEffectTableData',
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions,
-        showFilter: true,
-        enableColumnResize: true
-    };
-    //<!----end of VariantsEffect Table-->
-
-
-    //<!--------------Events---------------->
-    $scope.speciesChange  = function(){
-        ebiAppDomainHostService.speciesChangeBroadcast($scope.color.name);
-    }
-
-    //<!--------------Broadcast---------------->
-    $rootScope.$on('broadcastSpeciesChange', function() {
-            $scope.highchartsNG.series[0].data =  ebiAppDomainHostService.data;
-            $scope.highchartsNG.title.text =  ebiAppDomainHostService.message;
-
-            $scope.barChart.series[0].data =  ebiAppDomainHostService.data;
-            $scope.barChart.title.text =  ebiAppDomainHostService.message;
-
-    });
-
-    //<!--------------Broadcast---------------->
-    $rootScope.$on('VariantSelected', function() {
-        //$scope.variantEffectTableData =  variantEffectData;
-    });
-
 
 
 }]);
