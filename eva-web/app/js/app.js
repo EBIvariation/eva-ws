a344f93e86aef21d575d77ea60117d6d9b70ecee
kandaj <kandaj@ebi.ac.uk>
2014-06-13 15:47:04 +0100

    app js changed to www

--- eva-web/app/js/app.js
+++ eva-web/app/js/app.js
@@ -1,10 +1,10 @@
 
 
-METADATA_HOST = "http://wwwint.ebi.ac.uk/eva/webservices/rest";
+METADATA_HOST = "http://www.ebi.ac.uk/eva/webservices/rest";
 METADATA_VERSION = 'v1';
 
-CELLBASE_HOST = "http://ws-beta.bioinfo.cipf.es/cellbase-staging/rest";
-CELLBASE_VERSION = "latest";
+CELLBASE_HOST = "http://www.ebi.ac.uk/cellbase/webservices/rest";
+CELLBASE_VERSION = "v3";
 
 if(window.location.host.indexOf("wwwint.ebi.ac.uk") !== -1){
 
