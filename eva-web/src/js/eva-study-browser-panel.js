f0fbda717d243536e0b16a9dcc9d49e937b6bd25
kandaj <kandaj@ebi.ac.uk>
2014-10-08 14:22:37 +0100

    css chanes

--- eva-web/src/js/eva-study-browser-panel.js
+++ eva-web/src/js/eva-study-browser-panel.js
@@ -89,6 +89,7 @@ StudyBrowser.prototype = {
             defaults: {
                 margin: 5
             },
+            cls:'study-browser-panel',
             items: [this.toolTabPanel]
         });
 
