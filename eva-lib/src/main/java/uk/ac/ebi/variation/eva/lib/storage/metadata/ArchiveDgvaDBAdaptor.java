1fa2716a0f3ecc36c26017d23e4d4e7d622ba183
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-10-10 09:58:11 +0100

    Useless imports removed

--- eva-lib/src/main/java/uk/ac/ebi/variation/eva/lib/storage/metadata/ArchiveDgvaDBAdaptor.java
+++ eva-lib/src/main/java/uk/ac/ebi/variation/eva/lib/storage/metadata/ArchiveDgvaDBAdaptor.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
--- eva-lib/src/main/java/uk/ac/ebi/variation/eva/lib/storage/metadata/ArchiveEvaproDBAdaptor.java
+++ eva-lib/src/main/java/uk/ac/ebi/variation/eva/lib/storage/metadata/ArchiveEvaproDBAdaptor.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
