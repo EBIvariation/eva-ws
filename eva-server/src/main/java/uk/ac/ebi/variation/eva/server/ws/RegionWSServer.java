8e0fb3c57cca9245c4a3d4131ad98f03354d3f8b
Jacobo Coll Moragón <jacobo167@gmail.com>
2015-02-03 14:37:13 +0000

    server: updated QueryParams for VariantDBAdaptor

--- eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/RegionWSServer.java
+++ eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/RegionWSServer.java
@@ -11,6 +11,8 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
 import org.opencb.biodata.models.feature.Region;
 import org.opencb.opencga.lib.auth.IllegalOpenCGACredentialsException;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantDBAdaptor;
@@ -37,62 +39,46 @@ public class RegionWSServer extends EvaWSServer {
     public Response getVariantsByRegion(@PathParam("region") String regionId,
                                         @QueryParam("ref") String reference,
                                         @QueryParam("alt") String alternate,
-                                        @QueryParam("effects") String effects,
-                                        @QueryParam("studies") String studies,
                                         @QueryParam("species") String species,
-                                        @DefaultValue("-1f") @QueryParam("maf") float maf,
-                                        @DefaultValue("-1") @QueryParam("miss_alleles") int missingAlleles,
-                                        @DefaultValue("-1") @QueryParam("miss_gts") int missingGenotypes,
-                                        @DefaultValue("=") @QueryParam("maf_op") String mafOperator,
-                                        @DefaultValue("=") @QueryParam("miss_alleles_op") String missingAllelesOperator,
-                                        @DefaultValue("=") @QueryParam("miss_gts_op") String missingGenotypesOperator,
-                                        @DefaultValue("") @QueryParam("type") String variantType,
+                                        @DefaultValue("") @QueryParam("miss_alleles") String missingAlleles,
+                                        @DefaultValue("") @QueryParam("miss_gts") String missingGenotypes,
                                         @DefaultValue("false") @QueryParam("histogram") boolean histogram,
                                         @DefaultValue("-1") @QueryParam("histogram_interval") int interval,
                                         @DefaultValue("false") @QueryParam("merge") boolean merge) 
-            throws IllegalOpenCGACredentialsException, UnknownHostException, IOException {
-        
+            throws IllegalOpenCGACredentialsException, IOException {
+
         VariantDBAdaptor variantMongoDbAdaptor = DBAdaptorConnector.getVariantDBAdaptor(species);
-        
+
+        for (String acceptedValue : VariantDBAdaptor.QueryParams.acceptedValues) {
+            if (uriInfo.getQueryParameters().containsKey(acceptedValue)) {
+                List<String> values = uriInfo.getQueryParameters().get(acceptedValue);
+                String csv = values.get(0);
+                for (int i = 1; i < values.size(); i++) {
+                    csv += "," + values.get(i);
+                }
+                queryOptions.add(acceptedValue, csv);
+            }
+        }
+
         if (reference != null && !reference.isEmpty()) {
             queryOptions.put("reference", reference);
         }
         if (alternate != null && !alternate.isEmpty()) {
             queryOptions.put("alternate", alternate);
         }
-        if (effects != null && !effects.isEmpty()) {
-            queryOptions.put("effect", Arrays.asList(effects.split(",")));
-        }
-        if (studies != null && !studies.isEmpty()) {
-            queryOptions.put("studies", Arrays.asList(studies.split(",")));
-        }
         if (species != null && !species.isEmpty()) {
             queryOptions.put("species", species);
         }
-        if (!variantType.isEmpty()) {
-            queryOptions.put("type", variantType);
-        }
-        if (maf >= 0) {
-            queryOptions.put("maf", maf);
-            if (mafOperator != null) {
-                queryOptions.put("opMaf", mafOperator);
-            }
-        }
-        if (missingAlleles >= 0) {
+        if (!missingAlleles.isEmpty()) {
             queryOptions.put("missingAlleles", missingAlleles);
-            if (missingAllelesOperator != null) {
-                queryOptions.put("opMissingAlleles", missingAllelesOperator);
-            }
         }
-        if (missingGenotypes >= 0) {
+        if (!missingGenotypes.isEmpty()) {
             queryOptions.put("missingGenotypes", missingGenotypes);
-            if (missingGenotypesOperator != null) {
-                queryOptions.put("opMissingGenotypes", missingGenotypesOperator);
-            }
         }
+
         queryOptions.put("merge", merge);
-        
-        // Parse the provided regions. The total size of all regions together 
+
+        // Parse the provided regions. The total size of all regions together
         // can't excede 1 million positions
         int regionsSize = 0;
         List<Region> regions = new ArrayList<>();
@@ -101,9 +87,9 @@ public class RegionWSServer extends EvaWSServer {
             regions.add(r);
             regionsSize += r.getEnd() - r.getStart();
         }
-        
+
         if (histogram) {
-            if (regions.size() > 1) {
+            if (regions.size() != 1) {
                 return createErrorResponse("Sorry, histogram functionality only works with a single region");
             } else {
                 if (interval > 0) {
@@ -112,13 +98,21 @@ public class RegionWSServer extends EvaWSServer {
                 return createOkResponse(variantMongoDbAdaptor.getVariantFrequencyByRegion(regions.get(0), queryOptions));
             }
         } else if (regionsSize <= 1000000) {
-            return createOkResponse(variantMongoDbAdaptor.getAllVariantsByRegionList(regions, queryOptions));
+            if (regions.size() == 0) {
+                if (!queryOptions.containsKey("id") && !queryOptions.containsKey("gene")) {
+                    return createErrorResponse("Some positional filer is needed, like region, gene or id.");
+                } else {
+                    return createOkResponse(variantMongoDbAdaptor.getAllVariants(queryOptions));
+                }
+            } else {
+                return createOkResponse(variantMongoDbAdaptor.getAllVariantsByRegionList(regions, queryOptions));
+            }
         } else {
             return createErrorResponse("The total size of all regions provided can't exceed 1 million positions. "
                     + "If you want to browse a larger number of positions, please provide the parameter 'histogram=true'");
         }
     }
-    
+
     @OPTIONS
     @Path("/{region}/variants")
     public Response getVariantsByRegion() {
