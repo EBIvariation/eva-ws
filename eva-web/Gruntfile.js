90c5a26269a1f0e2373a6690dda40be4917c4086
kandaj <kandaj@ebi.ac.uk>
2015-02-03 09:50:02 +0000

    grunt js file upadted

--- eva-web/Gruntfile.js
+++ eva-web/Gruntfile.js
@@ -48,6 +48,8 @@ module.exports = function (grunt) {
                     'src/js/variant-widget/filters/eva-species-filter-form-panel.js',
                     'src/js/variant-widget/filters/eva-study-filter-form-panel.js',
                     'src/js/variant-widget/filters/eva-position-filter-form-panel.js',
+                    'src/js/variant-widget/filters/eva-popoulation-frequency-filter-form-panel.js',
+                    'src/js/variant-widget/filters/eva-protein-substitutions-score-filter-form-panel.js',
                     'src/js/study-browser/eva-study-browser-panel.js',
                     'src/js/study-browser/snv-study-browser.js',
                     'src/js/study-browser/sv-study-browser.js',
