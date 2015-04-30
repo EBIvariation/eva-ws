d6e7457ee744a68c2b0cf7e31577c1b06cd5aa36
kandaj <kandaj@ebi.ac.uk>
2015-04-30 14:09:10 +0100

    host name check conditions added

--- eva-web/src/js/eva-manager.js
+++ eva-web/src/js/eva-manager.js
@@ -25,7 +25,7 @@ if(window.location.protocol == 'https:'){
         METADATA_HOST = "https://wwwdev.ebi.ac.uk/eva/webservices/rest";
     }else if(document.location.host == 'wwwdev.ebi.ac.uk'){
         METADATA_HOST = "https://wwwdev.ebi.ac.uk/eva/webservices/rest";
-    }else if(document.location.host == 'www.ebi.ac.uk'){
+    }else if(document.location.host == 'www.ebi.ac.uk' || document.location.host == 'ves-oy-db.ebi.ac.uk' || document.location.host == 'ves-pg-db.ebi.ac.uk'){
         METADATA_HOST = "https://www.ebi.ac.uk/eva/webservices/rest";
     }else{
 //     METADATA_HOST = "http://172.22.70.137:8080/eva2/webservices/rest";
@@ -36,7 +36,7 @@ if(window.location.protocol == 'https:'){
        METADATA_HOST = "http://wwwdev.ebi.ac.uk/eva/webservices/rest";
    }else if(document.location.host == 'wwwdev.ebi.ac.uk'){
        METADATA_HOST = "http://wwwdev.ebi.ac.uk/eva/webservices/rest";
-   }else if(document.location.host == 'www.ebi.ac.uk'){
+   }else if(document.location.host == 'www.ebi.ac.uk' || document.location.host == 'ves-oy-db.ebi.ac.uk' || document.location.host == 'ves-pg-db.ebi.ac.uk'){
        METADATA_HOST = "http://www.ebi.ac.uk/eva/webservices/rest";
    }else{
 //     METADATA_HOST = "http://172.22.70.137:8080/eva2/webservices/rest";
