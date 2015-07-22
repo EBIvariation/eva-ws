a7b6dadcc7569b7b52d083825fa2deb8f5a2666d
kandaj <kandaj@ebi.ac.uk>
2015-07-22 13:56:03 +0100

    final push changes

--- eva-web/src/js/clinvar/eva-clinvar-annotation-panel.js
+++ eva-web/src/js/clinvar/eva-clinvar-annotation-panel.js
@@ -283,7 +283,7 @@ ClinvarAnnotationPanel.prototype = {
             });
         }else{
             var grid = Ext.create('Ext.view.View', {
-                tpl: new Ext.XTemplate(['<div>No Annotation data available</div>'])
+                tpl: new Ext.XTemplate(['<div style="margin-left:5px;">No Annotation data available</div>'])
             });
         }
 
