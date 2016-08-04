ef93f38b6dd9700fa785ad6921c4cb188c446e17
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-08-04 15:38:00 +0100

    More fine-grained Swagger configuration
    * Include paths under /webservices/rest
    * Scan classes only from the EVA package (exclude Spring /error endpoint)
    * Store Swagger JSON in /webservices/api
    
    The swagger-ui.html path can't be easily modified :(

--- eva-server/src/main/java/uk/ac/ebi/variation/eva/server/Application.java
+++ eva-server/src/main/java/uk/ac/ebi/variation/eva/server/Application.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -18,5 +25,25 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket apiConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/webservices/rest")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("uk.ac.ebi.variation.eva.server"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("European Variation Archive REST Web Services API")
+                .contact(new Contact("the European Variation Archive team", "www.ebi.ac.uk/eva", "eva-helpdesk@ebi.ac.uk"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0")
+                .build();
     }
 }
