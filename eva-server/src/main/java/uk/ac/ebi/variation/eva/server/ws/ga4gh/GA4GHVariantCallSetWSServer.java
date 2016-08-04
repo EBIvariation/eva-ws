c4e0b7c57c7775d4b5497aa5d75d0a9070eeecf3
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-08-04 10:35:19 +0100

    Migration to Spring REST API: GA4GHVariantCallSetWSServer done

--- eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/ga4gh/GA4GHVariantCallSetWSServer.java
+++ eva-server/src/main/java/uk/ac/ebi/variation/eva/server/ws/ga4gh/GA4GHVariantCallSetWSServer.java
@@ -2,7 +2,7 @@
  * European Variation Archive (EVA) - Open-access database of all types of genetic
  * variation data from all species
  *
- * Copyright 2014, 2015 EMBL - European Bioinformatics Institute
+ * Copyright 2014-2016 EMBL - European Bioinformatics Institute
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,16 +19,14 @@
 
 package uk.ac.ebi.variation.eva.server.ws.ga4gh;
 
-import io.swagger.annotations.Api;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.lang.StringUtils;
@@ -36,9 +34,20 @@ import org.opencb.biodata.ga4gh.GACallSet;
 import org.opencb.biodata.ga4gh.GASearchCallSetsRequest;
 import org.opencb.biodata.ga4gh.GASearchCallSetsResponse;
 import org.opencb.biodata.models.variant.ga4gh.GACallSetFactory;
+import org.opencb.datastore.core.QueryOptions;
 import org.opencb.datastore.core.QueryResult;
 import org.opencb.opencga.lib.auth.IllegalOpenCGACredentialsException;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantSourceDBAdaptor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
 import uk.ac.ebi.variation.eva.lib.datastore.DBAdaptorConnector;
 import uk.ac.ebi.variation.eva.server.ws.EvaWSServer;
 
@@ -46,31 +55,32 @@ import uk.ac.ebi.variation.eva.server.ws.EvaWSServer;
  *
  * @author Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
  */
-@Path("/v1/ga4gh/callsets")
-@Produces(MediaType.APPLICATION_JSON)
+@RestController
+@RequestMapping(value = "/v1/ga4gh/callsets", produces = "application/json")
 @Api(tags = { "ga4gh", "samples" })
 public class GA4GHVariantCallSetWSServer extends EvaWSServer {
+    
+    public GA4GHVariantCallSetWSServer() { }
     
     public GA4GHVariantCallSetWSServer(@Context UriInfo uriInfo, @Context HttpServletRequest hsr) {
         super(uriInfo, hsr);
     }
 
-    
-    @GET
-    @Path("/search")
     /**
      * 
      * @see http://ga4gh.org/documentation/api/v0.5/ga4gh_api.html#/schema/org.ga4gh.GASearchCallSetsRequest
      */
-    public Response getCallSets(@QueryParam("variantSetIds") String files,
-                                @QueryParam("pageToken") String pageToken,
-                                @DefaultValue("10") @QueryParam("pageSize") int limit,
-                                @DefaultValue("false") @QueryParam("histogram") boolean histogram,
-                                @DefaultValue("-1") @QueryParam("histogram_interval") int interval)
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public GASearchCallSetsResponse getCallSets(@RequestParam("variantSetIds") String files,
+                                                @RequestParam(name = "pageToken", required = false) String pageToken,
+                                                @RequestParam(name = "pageSize", defaultValue = "10") int limit,
+                                                @RequestParam(name = "histogram", defaultValue = "false") boolean histogram,
+                                                @RequestParam(name = "histogram_interval", defaultValue = "-1") int interval)
             throws UnknownHostException, IllegalOpenCGACredentialsException, IOException {
+        queryOptions = new QueryOptions();
         
-        if (files == null || files.isEmpty()) {
-            return createJsonUserErrorResponse("The 'variantSetIds' argument must not be empty");
+        if (files.isEmpty()) {
+            throw new IllegalArgumentException("The 'variantSetIds' argument must not be empty");
         }
         
         VariantSourceDBAdaptor dbAdaptor = DBAdaptorConnector.getVariantSourceDBAdaptor("hsapiens_grch37");
@@ -83,13 +93,7 @@ public class GA4GHVariantCallSetWSServer extends EvaWSServer {
         queryOptions.put("limit", limit);
         
         List<String> filesList = Arrays.asList(files.split(","));
-        QueryResult<List<String>> qr;
-        if (filesList.isEmpty()) {
-            // TODO This should accept a global search for all call sets (samples) in the DB
-            return createJsonUserErrorResponse("Please provide at least one variant set to search for");
-        } else {
-            qr = dbAdaptor.getSamplesBySources(filesList, queryOptions);
-        }
+        QueryResult<List<String>> qr = dbAdaptor.getSamplesBySources(filesList, queryOptions);
         
         // Convert sample names objects to GACallSet
         List<GACallSet> gaCallSets = GACallSetFactory.create(filesList, qr.getResult());
@@ -98,16 +102,20 @@ public class GA4GHVariantCallSetWSServer extends EvaWSServer {
         String nextPageToken = (idxLastElement < qr.getNumTotalResults()) ? String.valueOf(idxCurrentPage + 1) : null;
 
         // Create the custom response for the GA4GH API
-        return createJsonResponse(new GASearchCallSetsResponse(gaCallSets, nextPageToken));
+        return new GASearchCallSetsResponse(gaCallSets, nextPageToken);
     }
     
-    
-    @POST
-    @Path("/search")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response getCallSets(GASearchCallSetsRequest request)
+    @RequestMapping(value = "/search", method = RequestMethod.POST, consumes = "application/json")
+    public GASearchCallSetsResponse getCallSets(GASearchCallSetsRequest request)
             throws UnknownHostException, IllegalOpenCGACredentialsException, IOException {
         return getCallSets(StringUtils.join(request.getVariantSetIds(), ","), request.getPageToken(), request.getPageSize(), false, -1);
     }
     
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody String handleException(IllegalArgumentException e) {
+        // TODO Better to redirect to /error page with this status and message
+        return e.getMessage();
+    }
+
 }
