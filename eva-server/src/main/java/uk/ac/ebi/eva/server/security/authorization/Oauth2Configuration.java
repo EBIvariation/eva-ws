324aa03de14f1ac82024201bcc3bb2bf45f1e9c1
jorizci <jorizci@gmail.com>
2016-10-20 11:21:57 +0100

    Security parameter property to enable/disable, added a new package for security related components

---
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/security/authorization/Oauth2Configuration.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.eva.server.security.authorization;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+
+@ConditionalOnProperty(prefix = "eva.server.security", name = "enabled", havingValue = "true")
+@Configuration
+@EnableResourceServer
+public class Oauth2Configuration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.anonymous() // Enable anonymous / configure any related anonymous role
+                .and()
+                .authorizeRequests().antMatchers("/webservices/rest/swagger-ui.html**", "/webservices/rest/swagger-resources/**", "/webservices/rest/webjars/springfox-swagger-ui/**", "/webservices/rest/webservices/api").permitAll() //Authorize /hearbeat for everybody
+                .antMatchers("/**").authenticated() // The rest need to be authenticated
+        ;
+    }
+
+}
--- eva-server/src/main/java/uk/ac/ebi/eva/server/security/authorization/UnsecureConfiguration.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/security/authorization/UnsecureConfiguration.java
@@ -1,19 +1,22 @@
-package uk.ac.ebi.eva.server;
+package uk.ac.ebi.eva.server.security.authorization;
 
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@ConditionalOnProperty(prefix = "eva.server.security", name = "enabled", matchIfMissing = true, havingValue = "false")
 @Configuration
 @EnableResourceServer
-public class AuthorizationConfiguration extends ResourceServerConfigurerAdapter{
+public class UnsecureConfiguration extends ResourceServerConfigurerAdapter {
 
+    @Override
     public void configure(HttpSecurity http) throws Exception {
         http.anonymous() // Enable anonymous / configure any related anonymous role
                 .and()
-                .authorizeRequests().antMatchers("/webservices/rest/swagger-ui.html**", "/webservices/rest/swagger-resources/**","/webservices/rest/webjars/springfox-swagger-ui/**", "/webservices/rest/webservices/api").permitAll() //Authorize /hearbeat for everybody
-                .antMatchers("/**").authenticated() // The rest need to be authenticated
+                .authorizeRequests().antMatchers("/**").permitAll() //Authorize anonymous access to every mapping.
         ;
     }
 
