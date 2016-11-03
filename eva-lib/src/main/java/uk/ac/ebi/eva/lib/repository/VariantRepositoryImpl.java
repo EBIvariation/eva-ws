9cc3bff540c1d810cb41759946901e858015ac95
Tom Smith <tsmith@ebi.ac.uk>
2016-11-03 16:57:35 +0000

    VariantRepositoryImpl implementation of variant repository, needed due to use of converter.

---
+++ eva-lib/src/main/java/uk/ac/ebi/eva/lib/repository/VariantRepositoryImpl.java
@@ -0,0 +1,48 @@
+package uk.ac.ebi.eva.lib.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import uk.ac.ebi.eva.commons.models.converters.data.MongoDBObjectToVariantConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Tom Smith
+ */
+public class VariantRepositoryImpl {
+
+    @Autowired
+    private MongoDbFactory mongoDbFactory;
+
+    @Bean
+    public CustomConversions customConversions() {
+        List<Converter<?, ?>> converters = new ArrayList<Converter<?, ?>>();
+        converters.add(new MongoDBObjectToVariantConverter());
+        return new CustomConversions(converters);
+    }
+
+    @Bean
+    public MappingMongoConverter mongoConverter() {
+        MongoMappingContext mappingContext = new MongoMappingContext();
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mappingContext);
+        mongoConverter.setCustomConversions(customConversions());
+        mongoConverter.afterPropertiesSet();
+        return mongoConverter;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory, mongoConverter());
+    }
+
+}
