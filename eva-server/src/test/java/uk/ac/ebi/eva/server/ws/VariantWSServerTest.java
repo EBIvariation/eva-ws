623b050a2d8afbb34544b5a3389d01662e468a7f
Tom Smith <tsmith@ebi.ac.uk>
2016-11-04 15:20:04 +0000

    Helper methods. testGetVariantByRegionDoesntExist test.

--- eva-server/src/test/java/uk/ac/ebi/eva/server/ws/VariantWSServerTest.java
+++ eva-server/src/test/java/uk/ac/ebi/eva/server/ws/VariantWSServerTest.java
@@ -5,7 +5,6 @@ import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.opencb.biodata.models.variant.Variant;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -28,10 +27,8 @@ public class VariantWSServerTest {
         RestAssured.baseURI = "http://localhost:8080/eva/webservices/rest";
     }
 
-    @Test
-    public void testGetVariantById() throws URISyntaxException {
-        String testId = "rs567000874";
-        Response response = given().param("species", "mmusculus_grcm38").get(new URI("/v1/variants/" + testId + "/info"));
+    private void testGetVariantByIdRegionHelper(String testString) throws URISyntaxException {
+        Response response = given().param("species", "mmusculus_grcm38").get(new URI("/v1/variants/" + testString + "/info"));
         response.then().statusCode(200);
 
         List queryResponse = JsonPath.from(response.asString()).getList("response");
@@ -52,13 +49,30 @@ public class VariantWSServerTest {
         assertTrue(missingField, m.containsKey("length"));
         assertTrue(missingField, m.containsKey("reference"));
         assertTrue(missingField, m.containsKey("alternate"));
+    }
 
+    @Test
+    public void testGetVariantById() throws URISyntaxException {
+        testGetVariantByIdRegionHelper("rs567000874");
     }
 
     @Test
     public void testGetVariantByRegion() throws URISyntaxException {
-        String testRegion = "20:71822:C:G";
+        testGetVariantByIdRegionHelper("20:71822:C:G");
+    }
 
+    ///
+
+//    @Test
+//    public void testGetVariantByIdDoesntExist() throws URISyntaxException {
+//        String testId = "notarealid";
+//        Response response = given().param("species", "mmusculus_grcm38").get(new URI("/v1/variants/" + testId + "/info"));
+//        response.then().statusCode(200);
+//    }
+
+    @Test
+    public void testGetVariantByRegionDoesntExist() throws URISyntaxException {
+        String testRegion = "20:71821:C:G";
         Response response = given().param("species", "mmusculus_grcm38").get(new URI("/v1/variants/" + testRegion + "/info"));
         response.then().statusCode(200);
 
@@ -66,24 +80,12 @@ public class VariantWSServerTest {
         assertEquals(1, queryResponse.size());
 
         List<Map> result = JsonPath.from(response.asString()).getJsonObject("response[0].result");
-        assertTrue(result.size() == 1);
-
-        Map m = result.get(0);
-
-        String missingField = String.format("%s required field missing", m.get("id"));
-
-        assertTrue(missingField, m.containsKey("id"));
-        assertTrue(missingField, m.containsKey("type"));
-        assertTrue(missingField, m.containsKey("chromosome"));
-        assertTrue(missingField, m.containsKey("start"));
-        assertTrue(missingField, m.containsKey("end"));
-        assertTrue(missingField, m.containsKey("length"));
-        assertTrue(missingField, m.containsKey("reference"));
-        assertTrue(missingField, m.containsKey("alternate"));
-
+        assertTrue(result.size() == 0);
     }
 
-    private Boolean checkExists(String testRegion) throws URISyntaxException {
+    ///
+
+    private Boolean testCheckVariantExistsHelper(String testRegion) throws URISyntaxException {
         Response response = given().param("species", "mmusculus_grcm38").get(new URI("/v1/variants/" + testRegion + "/exists"));
         response.then().statusCode(200);
 
@@ -98,12 +100,12 @@ public class VariantWSServerTest {
 
     @Test
     public void testCheckVariantExistsDoesExist() throws URISyntaxException {
-        assertTrue(checkExists("20:71822:C:G"));
+        assertTrue(testCheckVariantExistsHelper("20:71822:C:G"));
     }
 
     @Test
     public void testCheckVariantExistsDoesntExist() throws URISyntaxException {
-        assertFalse(checkExists("20:71821:C:G"));
+        assertFalse(testCheckVariantExistsHelper("20:71821:C:G"));
     }
 
 
