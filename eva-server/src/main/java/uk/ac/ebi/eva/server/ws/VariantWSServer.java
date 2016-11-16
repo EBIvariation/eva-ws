dc1f701d8a37e66361365b280f24a67bb8d7f846
Tom Smith <tsmith@ebi.ac.uk>
2016-11-16 11:46:38 +0000

    reverted VariantWSServer changes to develop

--- eva-server/src/main/java/uk/ac/ebi/eva/server/ws/VariantWSServer.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/ws/VariantWSServer.java
@@ -21,25 +21,17 @@ package uk.ac.ebi.eva.server.ws;
 
 import io.swagger.annotations.Api;
 import org.opencb.biodata.models.feature.Region;
-import org.opencb.biodata.models.variant.Variant;
 import org.opencb.datastore.core.QueryResponse;
 import org.opencb.datastore.core.QueryResult;
 import org.opencb.opencga.lib.auth.IllegalOpenCGACredentialsException;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantDBAdaptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import uk.ac.ebi.eva.commons.models.metadata.VariantEntity;
-import uk.ac.ebi.eva.server.repository.VariantEntityRepository;
 import uk.ac.ebi.eva.lib.utils.DBAdaptorConnector;
-import uk.ac.ebi.eva.lib.utils.MultiMongoDbFactory;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -53,13 +45,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/v1/variants", produces = "application/json")
 @Api(tags = {"variants"})
-@EnableMongoRepositories("uk.ac.ebi.eva.server.repository")
 public class VariantWSServer extends EvaWSServer {
-
-    @Autowired
-    private VariantEntityRepository variantEntityRepository;
-
-    protected static Logger logger = LoggerFactory.getLogger(VariantWSServer.class);
 
     @RequestMapping(value = "/{variantId}/info", method = RequestMethod.GET)
 //    @ApiOperation(httpMethod = "GET", value = "Retrieves the information about a variant", response = QueryResponse.class)
@@ -68,47 +54,30 @@ public class VariantWSServer extends EvaWSServer {
                                         @RequestParam("species") String species,
                                         HttpServletResponse response)
             throws IllegalOpenCGACredentialsException, UnknownHostException, IOException {
-
         initializeQueryOptions();
 
+        VariantDBAdaptor variantMongoDbAdaptor = DBAdaptorConnector.getVariantDBAdaptor(species);
+
         if (studies != null && !studies.isEmpty()) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             queryOptions.put("studies", studies);
         }
 
         if (!variantId.contains(":")) { // Query by accession id
-            QueryResult<VariantEntity> queryResult = new QueryResult<>();
-            String dbName = DBAdaptorConnector.getDBName(species);
-            MultiMongoDbFactory.setDatabaseNameForCurrentThread(dbName);
-
-            List<VariantEntity> variants = variantEntityRepository.findByIds(variantId);
-            queryResult.setResult(variants);
-
-            return setQueryResponse(queryResult);
+            return setQueryResponse(variantMongoDbAdaptor.getVariantById(variantId, queryOptions));
         } else { // Query by chr:pos:ref:alt
-
-            VariantDBAdaptor variantMongoDbAdaptor = DBAdaptorConnector.getVariantDBAdaptor(species);
-
-            logger.warn("variantId: " + variantId);
-
             String parts[] = variantId.split(":", -1);
-            logger.warn("parts_0: " + parts[0]);
-            logger.warn("parts_1: " + parts[1]);
-            logger.warn("parts_2: " + parts[2]);
             if (parts.length < 3) {
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 return setQueryResponse("Invalid position and alleles combination, please use chr:pos:ref or chr:pos:ref:alt");
             }
 
             Region region = new Region(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[1]));
-            logger.warn("region: " + region);
             queryOptions.put("reference", parts[2]);
             if (parts.length > 3) {
                 queryOptions.put("alternate", String.join(":", Arrays.copyOfRange(parts, 3, parts.length)));
             }
 
             return setQueryResponse(variantMongoDbAdaptor.getAllVariantsByRegion(region, queryOptions));
-
         }
     }
 
