df9df45d1b2b75ee11b4f32d07710bfcf249e948
Tom Smith <tsmith@ebi.ac.uk>
2016-11-24 10:50:09 +0000

    Added sort to region query

--- eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepository.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepository.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.eva.server.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -14,6 +15,6 @@ public interface VariantEntityRepository extends MongoRepository<VariantEntity, 
     List<VariantEntity> findByIds(String id);
 
     @Query("{ 'chr' : ?0, 'start' : { $lt : ?2, $gt : $1 - " + VariantEntityRepository.MARGIN + " } , 'end' : { $gt : ?1, $lt : $2 + " + VariantEntityRepository.MARGIN + " }}")
-    List<VariantEntity> findByChrAndStartWithMarginAndEndWithMargin(String chromosome, int start, int end);
+    List<VariantEntity> findByChrAndStartWithMarginAndEndWithMargin(String chromosome, int start, int end, Sort sort);
 
 }
