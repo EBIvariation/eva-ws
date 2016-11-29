0de4ab8280593dd5fb151bed0bd9c76580051f11
Tom Smith <tsmith@ebi.ac.uk>
2016-11-29 14:12:48 +0000

    Better wrapping of documentation.

--- eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepositoryCustom.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepositoryCustom.java
@@ -23,7 +23,8 @@ import uk.ac.ebi.eva.commons.models.metadata.VariantEntity;
 import java.util.List;
 
 /**
- * Interface to declare additional repository methods with a custom implementation, instead of the one that Spring Data would provide by default.
+ * Interface to declare additional repository methods with a custom implementation,
+ * instead of the one that Spring Data would provide by default.
  */
 interface VariantEntityRepositoryCustom {
 
--- eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepositoryImpl.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/repository/VariantEntityRepositoryImpl.java
@@ -32,8 +32,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Concrete implementation of the VariantEntityRepository interface (relationship inferred by Spring), due to a custom DBObject to VariantEntity conversion
- * <p>It also implements the VariantEntityRepositoryCustom interface, to provide an explicit implementation of the region query, using a margin for efficiency.
+ * Concrete implementation of the VariantEntityRepository interface (relationship inferred by Spring),
+ * due to a custom DBObject to VariantEntity conversion
+ * <p>It also implements the VariantEntityRepositoryCustom interface,
+ * to provide an explicit implementation of the region query, using a margin for efficiency.
  */
 public class VariantEntityRepositoryImpl implements VariantEntityRepositoryCustom {
 
