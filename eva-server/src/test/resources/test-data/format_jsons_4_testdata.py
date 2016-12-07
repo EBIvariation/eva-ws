7b34f596533264d9fc4f4d92adbb419f5666e838
Tom Smith <tsmith@ebi.ac.uk>
2016-12-07 09:43:04 +0000

    - Added information on removing src object from test json data

--- eva-server/src/test/resources/test-data/format_jsons_4_testdata.py
+++ eva-server/src/test/resources/test-data/format_jsons_4_testdata.py
@@ -3,12 +3,15 @@ import os
 import io
 import sys
 
-# Script used to convert a file with one JSON per line,
-# to a file suitable for Lord of the Jars to use as a test
-# dataset to load to a testing Mongo database.
+# Script used to convert a file with one JSON per line, to a file suitable for Lord of the Jars 
+# to use as a test dataset to load to a testing Mongo database.
 # Lord of the Jars requires a json file in the format:
 # {collection_name:[...documents for entry into collection...]}
-
+#
+# JSONs cannot be loaded if they have the src field, since the Java MongoDB driver cannot 
+# currently handle the $type value of $binary specified as a hexadecimal string (the format output
+# by mongoexport). Due to this, the src object is removed. The bug in the driver has been fixed and
+# will be available in a future driver version.
 
 with open(sys.argv[1], "rt") as infile:
     with open(sys.argv[2], "wt") as outfile:
