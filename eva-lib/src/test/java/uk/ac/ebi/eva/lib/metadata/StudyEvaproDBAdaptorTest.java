e2e669174a984c37b0a19eb2ad61f92157fc4f42
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-02-14 09:57:50 +0000

    StudyEvaProDBAdaptor: extracted test data to separate class

--- eva-lib/src/test/java/uk/ac/ebi/eva/lib/metadata/StudyEvaproDBAdaptorTest.java
+++ eva-lib/src/test/java/uk/ac/ebi/eva/lib/metadata/StudyEvaproDBAdaptorTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import uk.ac.ebi.eva.lib.entity.EvaStudyBrowser;
 import uk.ac.ebi.eva.lib.models.VariantStudy;
 import uk.ac.ebi.eva.lib.utils.QueryOptionsConstants;
 
@@ -30,42 +29,10 @@ public class StudyEvaproDBAdaptorTest {
     @Autowired
     private StudyEvaproDBAdaptor studyEvaproDBAdaptor;
 
-    private static final String HUMAN = "human";
-
-    private static final String EXOME_SEQUENCING = "Exome sequencing";
-
-    private static final String PROJECT_ID_1 = "PRJ0001";
-
     @Before
     public void setUp() throws Exception {
-        EvaStudyBrowser humanWGSStudy1 = new EvaStudyBrowser(PROJECT_ID_1, 1, "Project 1", "This is project 1", "1111",
-                                                             HUMAN, "Homo sapiens", "source 1",
-                                                             "Whole genome sequencing", 1000000L, 1000, "center 1",
-                                                             "scope 1", "DNA", "PubMed:1111", "PRJ00002",
-                                                             "whole genome seq", "WGS", "GCA_000001405.22", "GRCh38.p7",
-                                                             "Illumina", "Resource1");
-        EvaStudyBrowser humanWGSStudy2 = new EvaStudyBrowser("PRJ0002", 2, "Project 2", "This is project 2", "2222",
-                                                             HUMAN, "Homo sapiens", "source 2",
-                                                             "Whole genome sequencing", 2000000L, 2000,
-                                                             "center 2", "scope 2", "DNA", "PubMed:2222", "PRJ00002",
-                                                             "whole genome seq", "WGS", "GCA_000001405.22", "GRCh38.p7",
-                                                             "Illumina", "Resource2");
-        EvaStudyBrowser humanESStudy = new EvaStudyBrowser("PRJ0003", 3, "Project 3", "This is project 3", "3333",
-                                                           HUMAN, "Homo sapiens", "source 3", EXOME_SEQUENCING,
-                                                           3000000L, 3000,
-                                                           "center 3", "scope 3", "DNA", "PubMed:3333", "PRJ00003",
-                                                           EXOME_SEQUENCING, "ES", "GCA_000001405.14", "GRCh37.p13",
-                                                           "Illumina", "Resource3");
-        EvaStudyBrowser cowESStudy = new EvaStudyBrowser("PRJ0004", 4, "Project 4", "This is project 4", "4444",
-                                                         "cow", "Bos taurus", "source 4", EXOME_SEQUENCING, 4000000L,
-                                                         4000,
-                                                         "center 4", "scope 4", "DNA", "PubMed:4444", "PRJ00004",
-                                                         EXOME_SEQUENCING, "ES", "GCA_000003055.3", "UMD3.1",
-                                                         "Illumina", "Resource4");
-        entityManager.persist(humanWGSStudy1);
-        entityManager.persist(humanWGSStudy2);
-        entityManager.persist(humanESStudy);
-        entityManager.persist(cowESStudy);
+        EvaStudyBrowserTestData testData = new EvaStudyBrowserTestData(entityManager);
+        testData.persistTestData();
     }
 
     @After
@@ -83,31 +50,34 @@ public class StudyEvaproDBAdaptorTest {
     @Test
     public void getAllStudiesForOneSpecies() throws Exception {
         QueryOptions queryOptions = new QueryOptions();
-        queryOptions.put(QueryOptionsConstants.SPECIES, HUMAN);
+        queryOptions.put(QueryOptionsConstants.SPECIES, EvaStudyBrowserTestData.HUMAN);
         QueryResult<VariantStudy> queryResult = studyEvaproDBAdaptor.getAllStudies(queryOptions);
 
-        checkReturnedStudies(queryResult, 3, study -> study.getSpeciesCommonName().equals(HUMAN));
+        checkReturnedStudies(queryResult, 3,
+                             study -> study.getSpeciesCommonName().equals(EvaStudyBrowserTestData.HUMAN));
     }
 
     @Test
     public void getAllStudiesForOneType() throws Exception {
         QueryOptions queryOptions = new QueryOptions();
-        queryOptions.put(QueryOptionsConstants.TYPE, EXOME_SEQUENCING);
+        queryOptions.put(QueryOptionsConstants.TYPE, EvaStudyBrowserTestData.EXOME_SEQUENCING);
         QueryResult<VariantStudy> queryResult = studyEvaproDBAdaptor.getAllStudies(queryOptions);
 
-        checkReturnedStudies(queryResult, 2, study -> study.getExperimentType().equals(EXOME_SEQUENCING));
+        checkReturnedStudies(queryResult, 2,
+                             study -> study.getExperimentType().equals(EvaStudyBrowserTestData.EXOME_SEQUENCING));
     }
 
     @Test
     public void getAllStudiesForAnSpeciesAndType() throws Exception {
         QueryOptions queryOptions = new QueryOptions();
-        queryOptions.put(QueryOptionsConstants.SPECIES, HUMAN);
-        queryOptions.put(QueryOptionsConstants.TYPE, EXOME_SEQUENCING);
+        queryOptions.put(QueryOptionsConstants.SPECIES, EvaStudyBrowserTestData.HUMAN);
+        queryOptions.put(QueryOptionsConstants.TYPE, EvaStudyBrowserTestData.EXOME_SEQUENCING);
         QueryResult<VariantStudy> queryResult = studyEvaproDBAdaptor.getAllStudies(queryOptions);
 
         checkReturnedStudies(queryResult, 1,
-                             study -> study.getSpeciesCommonName().equals(HUMAN) && study.getExperimentType()
-                                                                                         .equals(EXOME_SEQUENCING));
+                             study -> study.getSpeciesCommonName().equals(EvaStudyBrowserTestData.HUMAN) && study
+                                     .getExperimentType()
+                                     .equals(EvaStudyBrowserTestData.EXOME_SEQUENCING));
     }
 
     @Test(expected = UnsupportedOperationException.class)
@@ -122,9 +92,10 @@ public class StudyEvaproDBAdaptorTest {
 
     @Test
     public void getStudyById() throws Exception {
-        QueryResult<VariantStudy> queryResult = studyEvaproDBAdaptor.getStudyById(PROJECT_ID_1, new QueryOptions());
+        QueryResult<VariantStudy> queryResult = studyEvaproDBAdaptor
+                .getStudyById(EvaStudyBrowserTestData.PROJECT_ID_1, new QueryOptions());
 
-        checkReturnedStudies(queryResult, 1, study -> study.getId().equals(PROJECT_ID_1));
+        checkReturnedStudies(queryResult, 1, study -> study.getId().equals(EvaStudyBrowserTestData.PROJECT_ID_1));
     }
 
     @Test(expected = UnsupportedOperationException.class)
