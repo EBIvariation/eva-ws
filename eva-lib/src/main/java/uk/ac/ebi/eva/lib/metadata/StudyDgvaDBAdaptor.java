f9faf0c70ef6298c2511dc0c9a6d56e568bd911c
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-02-15 09:28:04 +0000

    Removed unused filter in StudyDgvaDBAdaptor

--- eva-lib/src/main/java/uk/ac/ebi/eva/lib/metadata/StudyDgvaDBAdaptor.java
+++ eva-lib/src/main/java/uk/ac/ebi/eva/lib/metadata/StudyDgvaDBAdaptor.java
@@ -50,7 +50,6 @@ public class StudyDgvaDBAdaptor implements StudyDBAdaptor {
     @Override
     public QueryResult getStudyById(String studyId, QueryOptions queryOptions) {
         long start = System.currentTimeMillis();
-        Specification filterSpecification = DgvaDBUtils.getSpeciesAndTypeFilters(queryOptions);
         DgvaStudyBrowser dgvaStudy = dgvaStudyBrowserRepository.getOne(studyId);
         List<VariantStudy> variantStudy = new ArrayList<>();
         if (dgvaStudy != null) {
