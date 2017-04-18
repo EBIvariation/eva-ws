670e41b8d29f463c5b8e457d29d2c959a6a8132e
Tom Smith <tsmith@ebi.ac.uk>
2017-04-18 13:39:43 +0100

    Test loading of properties using class against value annotation

--- eva-lib/src/test/java/uk/ac/ebi/eva/lib/config/EvaPropertyTest.java
+++ eva-lib/src/test/java/uk/ac/ebi/eva/lib/config/EvaPropertyTest.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.eva.lib.config;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -25,10 +26,14 @@ public class EvaPropertyTest {
     @Autowired
     private EvaProperty evaProperty;
 
+    @Value("${eva.mongo.host}")
+    private String expectedHostProp;
+
     @Test
     public void testEvaPropertyAutowiring() {
         assertNotNull(evaProperty);
         assertNotNull(evaProperty.getMongo());
+        assertEquals(expectedHostProp, evaProperty.getMongo().getHost());
     }
 
 }
