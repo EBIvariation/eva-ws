7c330373584a173cc25e57ef632421250c8da857
Tom Smith <tsmith@ebi.ac.uk>
2017-06-06 16:29:14 +0100

    - startTime and endTime to protected - Removed author annotations

--- dgva-server/src/main/java/uk/ac/ebi/dgva/server/Profiles.java
+++ dgva-server/src/main/java/uk/ac/ebi/dgva/server/Profiles.java
@@ -16,9 +16,6 @@
 
 package uk.ac.ebi.dgva.server;
 
-/**
- * Created by jorizci on 01/11/16.
- */
 public class Profiles {
     public static final String OAUTH_SECURITY = "oauth2-security";
     public static final String NOT_OAUTHSECURITY = "!"+ OAUTH_SECURITY;
