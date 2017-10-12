51e39a364b6b3bd161d0baa6f228b65f7bcce3d7
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2017-10-12 09:20:36 +0100

    Support for WAR deployment in Tomcat

--- dbsnp-import/src/main/java/uk/ac/ebi/eva/server/Application.java
+++ dbsnp-import/src/main/java/uk/ac/ebi/eva/server/Application.java
@@ -18,9 +18,17 @@ package uk.ac.ebi.eva.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
