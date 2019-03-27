da5460a59048547e93165350e277044dd695f153
Andres Silva <afsilva04@gmail.com>
2019-03-27 11:27:34 +0000

    add copyright, return iterator instead of a list

--- dbsnp-import/src/main/java/uk/ac/ebi/eva/server/controllers/DbsnpRestController.java
+++ dbsnp-import/src/main/java/uk/ac/ebi/eva/server/controllers/DbsnpRestController.java
@@ -1,14 +1,26 @@
+/*
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ebi.eva.server.controllers;
 
-import com.google.common.collect.Lists;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.ebi.eva.server.models.ProgressReport;
 import uk.ac.ebi.eva.server.repositories.ProgressReportRepository;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1/import-status", produces = "application/json")
@@ -21,8 +33,7 @@ public class DbsnpRestController {
     }
 
     @GetMapping
-    public List<ProgressReport> getReport() {
-        List<ProgressReport> progressReports = Lists.newArrayList(repository.findAll());
-        return progressReports;
+    public Iterable<ProgressReport> getReport() {
+        return repository.findAll();
     }
 }
