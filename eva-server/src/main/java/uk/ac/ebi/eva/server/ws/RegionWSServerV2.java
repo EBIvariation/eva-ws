4f3b82a93182ae6f6ae46159b9474c22f93baa02
junaidnz97 <junaidnz97@gmail.com>
2019-07-03 21:34:18 +0530

    split species into species and assembly

--- eva-server/src/main/java/uk/ac/ebi/eva/server/ws/RegionWSServerV2.java
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/ws/RegionWSServerV2.java
@@ -20,20 +20,20 @@
 package uk.ac.ebi.eva.server.ws;
 
 import io.swagger.annotations.Api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.ApiIgnore;
 
 import uk.ac.ebi.eva.commons.core.models.AnnotationMetadata;
 import uk.ac.ebi.eva.commons.core.models.Region;
+import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
 import uk.ac.ebi.eva.commons.core.models.ws.VariantWithSamplesAndAnnotation;
 import uk.ac.ebi.eva.commons.mongodb.filter.FilterBuilder;
 import uk.ac.ebi.eva.commons.mongodb.filter.VariantRepositoryFilter;
@@ -41,24 +41,19 @@ import uk.ac.ebi.eva.commons.mongodb.services.AnnotationMetadataNotFoundExceptio
 import uk.ac.ebi.eva.commons.mongodb.services.VariantWithSamplesAndAnnotationsService;
 import uk.ac.ebi.eva.lib.eva_utils.DBAdaptorConnector;
 import uk.ac.ebi.eva.lib.eva_utils.MultiMongoDbFactory;
-import uk.ac.ebi.eva.lib.utils.QueryResponse;
-import uk.ac.ebi.eva.lib.utils.QueryResult;
 import uk.ac.ebi.eva.server.RateLimit;
 import uk.ac.ebi.eva.server.Utils;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v2/segments", produces = "application/json")
 @Api(tags = {"segments"})
-public class RegionWSServerV2 extends EvaWSServer {
+public class RegionWSServerV2 {
 
     private static final int REGION_REQUEST_RATE_LIMIT = 5;
-    protected static Logger logger = LoggerFactory.getLogger(FeatureWSServer.class);
+
     @Autowired
     private VariantWithSamplesAndAnnotationsService service;
 
@@ -68,41 +63,34 @@ public class RegionWSServerV2 extends EvaWSServer {
     @RequestMapping(value = "/{regionId}/variants", method = RequestMethod.GET)
     @ResponseBody
     @RateLimit(value = REGION_REQUEST_RATE_LIMIT)
-    public QueryResponse getVariantsByRegion(@PathVariable("regionId") String regionId,
-                                             @RequestParam(name = "species") String species,
-                                             @RequestParam(name = "studies", required = false) List<String> studies,
-                                             @RequestParam(name = "annot-ct", required = false)
-                                                         List<String> consequenceType,
-                                             @RequestParam(name = "maf", required = false) String maf,
-                                             @RequestParam(name = "polyphen", required = false) String polyphenScore,
-                                             @RequestParam(name = "sift", required = false) String siftScore,
-                                             @RequestParam(name = "exclude", required = false) List<String> exclude,
-                                             @RequestParam(name = "annot-vep-version", required = false)
-                                                         String annotationVepVersion,
-                                             @RequestParam(name = "annot-vep-cache-version", required = false)
-                                                         String annotationVepCacheVersion,
-                                             HttpServletResponse response,
-                                             @ApiIgnore HttpServletRequest request)
-            throws IOException {
-        initializeQuery();
+    public ResponseEntity getVariantsByRegion(@PathVariable("regionId") String regionId,
+                                              @RequestParam(name = "species") String species,
+                                              @RequestParam(name = "assembly") String assembly,
+                                              @RequestParam(name = "studies", required = false) List<String> studies,
+                                              @RequestParam(name = "annot-ct", required = false)
+                                                      List<String> consequenceType,
+                                              @RequestParam(name = "maf", required = false) String maf,
+                                              @RequestParam(name = "polyphen", required = false) String polyphenScore,
+                                              @RequestParam(name = "sift", required = false) String siftScore,
+                                              @RequestParam(name = "exclude", required = false) List<String> exclude,
+                                              @RequestParam(name = "annot-vep-version", required = false)
+                                                      String annotationVepVersion,
+                                              @RequestParam(name = "annot-vep-cache-version", required = false)
+                                                      String annotationVepCacheVersion)
+            throws IllegalArgumentException {
+        checkParameters(annotationVepVersion, annotationVepCacheVersion, species, exclude);
 
-        String errorMessage = checkErrorHelper(annotationVepVersion, annotationVepCacheVersion, species, exclude);
-        if (errorMessage != null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return setQueryResponse(errorMessage);
-        }
-
-        MultiMongoDbFactory.setDatabaseNameForCurrentThread(DBAdaptorConnector.getDBName(species));
+        MultiMongoDbFactory.setDatabaseNameForCurrentThread(DBAdaptorConnector.getDBName(species + "_" + assembly));
 
         List<VariantRepositoryFilter> filters = new FilterBuilder()
                 .getVariantEntityRepositoryFilters(maf, polyphenScore, siftScore, studies, consequenceType);
-        List<Region> regions = Region.parseRegions(regionId);
-        PageRequest pageRequest = Utils.getPageRequest(getQueryOptions());
 
+        List<Region> regions = Region.parseRegions(regionId);
         List<String> excludeMapped = getExcludeMapped(exclude);
 
         AnnotationMetadata annotationMetadata = getAnnotationMetadataHelper(annotationVepVersion,
                 annotationVepCacheVersion);
+        Integer pageSize = service.countByRegionsAndComplexFilters(regions, filters).intValue();
 
         List<VariantWithSamplesAndAnnotation> variantEntities;
 
@@ -111,43 +99,39 @@ public class RegionWSServerV2 extends EvaWSServer {
                     filters,
                     annotationMetadata,
                     excludeMapped,
-                    pageRequest);
+                    new PageRequest(0, pageSize));
         } catch (AnnotationMetadataNotFoundException ex) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return setQueryResponse(ex.getMessage());
+            return new ResponseEntity(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
-        Long numTotalResults = service.countByRegionsAndComplexFilters(regions, filters);
-
-        List<VariantWithSamplesAndAnnotation> requiredVariantEntities = new ArrayList<>();
+        List<Variant> variantCoreInfo = new ArrayList<>();
 
         variantEntities.forEach(variantEntity -> {
-            requiredVariantEntities.add(new VariantWithSamplesAndAnnotation(variantEntity.getChromosome(),
-                    variantEntity.getStart(), variantEntity.getEnd(), variantEntity.getReference(),
-                    variantEntity.getAlternate(),
-                    null));
+            Variant variant = new Variant(variantEntity.getChromosome(), variantEntity.getStart(),
+                    variantEntity.getEnd(), variantEntity.getReference(), variantEntity.getAlternate());
+            variant.setIds(variantEntity.getIds());
+            variantCoreInfo.add(variant);
         });
 
-        QueryResult<VariantWithSamplesAndAnnotation> queryResult = buildQueryResult(requiredVariantEntities,
-                numTotalResults);
-        return setQueryResponse(queryResult);
+        return new ResponseEntity(variantCoreInfo, HttpStatus.OK);
     }
 
-    public String checkErrorHelper(String annotationVepVersion, String annotationVepCacheVersion, String species,
-                                   List<String> exclude) {
+    public String checkParameters(String annotationVepVersion, String annotationVepCacheVersion, String species,
+                                  List<String> exclude) throws IllegalArgumentException {
         if (annotationVepVersion == null ^ annotationVepCacheVersion == null) {
-            return "Please specify either both annotation VEP version and annotation VEP cache version, or neither";
+            throw new IllegalArgumentException("Please specify either both annotation VEP version and annotation VEP" +
+                    " cache version, or neither");
         }
 
         if (species.isEmpty()) {
-            return "Please specify a species";
+            throw new IllegalArgumentException("Please specify a species");
         }
 
         if (exclude != null && !exclude.isEmpty()) {
             for (String e : exclude) {
                 String docPath = Utils.getApiToMongoDocNameMap().get(e);
                 if (docPath == null) {
-                    return "Unrecognised exclude field: " + e;
+                    throw new IllegalArgumentException("Unrecognised exclude field: " + e);
                 }
             }
         }
