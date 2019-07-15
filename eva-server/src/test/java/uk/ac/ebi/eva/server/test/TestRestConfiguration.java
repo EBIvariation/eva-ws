1f6e95ace2d5be4f6300424ed681d038862722f1
junaidnz97 <junaidnz97@gmail.com>
2019-07-15 21:23:48 +0530

    change in config

--- eva-server/src/test/java/uk/ac/ebi/eva/server/test/TestRestConfiguration.java
+++ eva-server/src/test/java/uk/ac/ebi/eva/server/test/TestRestConfiguration.java
@@ -21,18 +21,28 @@ package uk.ac.ebi.eva.server.test;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.hateoas.hal.Jackson2HalModule;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class TestRestConfiguration {
 
     @Bean
-    public RestTemplateBuilder restTemplateBuilder() {
+    public RestTemplateBuilder restTemplateBuilder(
+            ObjectProvider<HttpMessageConverters> messageConverters,
+            ObjectProvider<List<RestTemplateCustomizer>> restTemplateCustomizers) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.registerModule(new Jackson2HalModule());
@@ -41,6 +51,20 @@ public class TestRestConfiguration {
         converter.setObjectMapper(mapper);
 
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-        return restTemplateBuilder.additionalMessageConverters(converter);
+
+        HttpMessageConverters converters = (HttpMessageConverters) messageConverters.getIfUnique();
+        restTemplateBuilder.additionalMessageConverters(converter);
+        if (converters != null) {
+            restTemplateBuilder.additionalMessageConverters(converters.getConverters());
+        }
+        List<RestTemplateCustomizer> customizers = (List) restTemplateCustomizers.getIfAvailable();
+        if (!CollectionUtils.isEmpty(customizers)) {
+            customizers = new ArrayList(customizers);
+            AnnotationAwareOrderComparator.sort(customizers);
+            restTemplateBuilder = restTemplateBuilder.customizers(customizers);
+        }
+
+        return restTemplateBuilder;
+
     }
 }
