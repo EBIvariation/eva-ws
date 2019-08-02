ee947cc84216a2ec9b89f75e3c998fa233f691ee
junaidnz97 <junaidnz97@gmail.com>
2019-08-02 16:00:49 +0530

    Added archive V2 endpoint with tests

---
+++ eva-server/src/main/java/uk/ac/ebi/eva/server/ws/ArchiveWSServerV2.java
@@ -0,0 +1,102 @@
+package uk.ac.ebi.eva.server.ws;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.eva.commons.mongodb.entities.projections.VariantStudySummary;
+import uk.ac.ebi.eva.commons.mongodb.services.VariantStudySummaryService;
+import uk.ac.ebi.eva.lib.eva_utils.DBAdaptorConnector;
+import uk.ac.ebi.eva.lib.eva_utils.MultiMongoDbFactory;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping(value = "/v2/meta", produces = "application/hal+json")
+@Api(tags = "archive")
+public class ArchiveWSServerV2 {
+
+    @Autowired
+    private VariantStudySummaryService variantStudySummaryService;
+
+    @RequestMapping(value = "/studies", method = RequestMethod.GET)
+    public ResponseEntity getBrowsableStudies(@RequestParam("species") String species,
+                                              @RequestParam("assembly") String assembly,
+                                              @RequestParam(required = false, defaultValue = "0") Integer pageNumber,
+                                              @RequestParam(required = false, defaultValue = "20") Integer pageSize)
+            throws IllegalArgumentException {
+        if (species == null || species.isEmpty()) {
+            throw new IllegalArgumentException("Please specify a species");
+        }
+
+        MultiMongoDbFactory.setDatabaseNameForCurrentThread(DBAdaptorConnector.getDBName(species + "_" + assembly));
+
+        int totalNumberOfResults = variantStudySummaryService.countAll();
+        if (totalNumberOfResults == 0) {
+            return new ResponseEntity(new PagedResources<>(Collections.EMPTY_LIST, new PagedResources.PageMetadata
+                    (pageSize, pageNumber < 0 ? 0 : pageNumber, totalNumberOfResults)), HttpStatus.NO_CONTENT);
+        }
+
+        List<VariantStudySummary> uniqueStudies = variantStudySummaryService.findAll(pageNumber, pageSize);
+
+        PagedResources.PageMetadata pageMetadata;
+        try {
+            pageMetadata = buildPageMetadata(pageSize, pageNumber, totalNumberOfResults);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE);
+        }
+
+        PagedResources pagedResources = buildPagedResources(uniqueStudies, species, assembly, pageMetadata);
+        return new ResponseEntity(pagedResources, HttpStatus.OK);
+    }
+
+    private PagedResources.PageMetadata buildPageMetadata(Integer pageSize, Integer pageNumber, Integer totalNumberOfResults)
+            throws IllegalArgumentException {
+        Long totalPages = pageSize == 0L ? 0L : (long) Math.ceil((double) totalNumberOfResults / (double) pageSize);
+
+        if (pageNumber < 0 || pageNumber >= totalPages) {
+            throw new IllegalArgumentException("For the given page size, there are " + totalPages + " page(s), so the" +
+                    " correct page range is from 0 to " + String.valueOf(totalPages - 1) + " (both included).");
+        }
+        return new PagedResources.PageMetadata(pageSize, pageNumber, totalNumberOfResults, totalPages);
+    }
+
+    private PagedResources buildPagedResources(List<VariantStudySummary> uniqueStudies, String species,
+                                               String assembly, PagedResources.PageMetadata pageMetadata) {
+
+        PagedResources pagedResources = new PagedResources<>(uniqueStudies, pageMetadata);
+
+        int pageNumber = (int) pageMetadata.getNumber();
+        int pageSize = (int) pageMetadata.getSize();
+
+        if (pageNumber > 0) {
+            pagedResources.add(createPaginationLink(species, assembly, pageNumber - 1, pageSize, "prev"));
+
+            pagedResources.add(createPaginationLink(species, assembly, 0, pageSize, "first"));
+        }
+
+        if (pageNumber < (pageMetadata.getTotalPages() - 1)) {
+            pagedResources.add(createPaginationLink(species, assembly, pageNumber + 1, pageSize, "next"));
+
+            pagedResources.add(createPaginationLink(species, assembly, (int) pageMetadata.getTotalPages() - 1, pageSize, "last"));
+        }
+        return pagedResources;
+    }
+
+    private Link createPaginationLink(String species, String assembly, int pageNumber, int pageSize, String linkName) {
+        return new Link(linkTo(methodOn(ArchiveWSServerV2.class).getBrowsableStudies(species, assembly,
+                pageNumber, pageSize))
+                .toUriComponentsBuilder()
+                .toUriString(), linkName);
+    }
+}
---
+++ eva-server/src/test/java/uk/ac/ebi/eva/server/ws/ArchiveWSServerV2Test.java
@@ -0,0 +1,86 @@
+package uk.ac.ebi.eva.server.ws;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.TypeRef;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.eva.commons.mongodb.entities.projections.VariantStudySummary;
+import uk.ac.ebi.eva.commons.mongodb.services.VariantStudySummaryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.eq;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ArchiveWSServerV2Test {
+
+    private static final int PAGE_SIZE = 2;
+
+    private static final int PAGE_NUMBER = 0;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @MockBean
+    private VariantStudySummaryService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        List<VariantStudySummary> studies = new ArrayList<>();
+        VariantStudySummary study1 = new VariantStudySummary();
+        VariantStudySummary study2 = new VariantStudySummary();
+
+        study1.setFilesCount(1);
+        study1.setStudyId("studyId1");
+        study1.setStudyName("studyName1");
+        studies.add(study1);
+
+        study2.setFilesCount(2);
+        study2.setStudyId("studyId2");
+        study2.setStudyName("studyName2");
+        studies.add(study2);
+
+        given(service.findAll(eq(PAGE_NUMBER), eq(PAGE_SIZE))).willReturn(studies);
+        given(service.countAll()).willReturn(2);
+    }
+
+    @Test
+    public void testGetStudies() {
+        String url = "/v2/meta/studies?species=mmusculus&assembly=grcm38&pageNumber=0&pageSize=2";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Configuration configuration = Configuration.defaultConfiguration()
+                .jsonProvider(new JacksonJsonProvider())
+                .mappingProvider(new JacksonMappingProvider(objectMapper))
+                .addOptions(Option.SUPPRESS_EXCEPTIONS);
+
+        List<VariantStudySummary> variantList = JsonPath.using(configuration).parse(response.getBody())
+                .read("$['_embedded']['variantStudySummaryList']", new TypeRef<List<VariantStudySummary>>() {
+                });
+        assertEquals("studyId1", variantList.get(0).getStudyId());
+        assertEquals("studyName1", variantList.get(0).getStudyName());
+        assertEquals("studyId2", variantList.get(1).getStudyId());
+        assertEquals("studyName2", variantList.get(1).getStudyName());
+    }
+}
