8f118b64ef69e39206cb67c96c19c7f2147fd79b
junaidnz97 <junaidnz97@gmail.com>
2019-08-28 18:42:57 +0530

    Added tests for additions and deletions

--- eva-server/src/test/java/uk/ac/ebi/eva/server/ws/VariantWSServerV2IntegrationTest.java
+++ eva-server/src/test/java/uk/ac/ebi/eva/server/ws/VariantWSServerV2IntegrationTest.java
@@ -210,4 +210,31 @@ public class VariantWSServerV2IntegrationTest {
         assertEquals("Please specify either both annotation VEP version and annotation VEP cache version, " +
                 "or neither", testForErrorHelper(url));
     }
+
+    @Test
+    public void rootTestForAdditions() throws URISyntaxException {
+        String url = "/v2/variants/13:32889711:T:?species=mmusculus&assembly=grcm38";
+        VariantWithSamplesAndAnnotation variantWithSamplesAndAnnotations = restTemplate.exchange(
+                url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<VariantWithSamplesAndAnnotation>() {
+                }).getBody();
+        assertEquals("13", variantWithSamplesAndAnnotations.getChromosome());
+        assertEquals("T", variantWithSamplesAndAnnotations.getReference());
+        assertNull(variantWithSamplesAndAnnotations.getAlternate());
+        assertEquals(32889711, variantWithSamplesAndAnnotations.getStart());
+    }
+
+    @Test
+    public void rootTestForDeletions() throws URISyntaxException {
+        String url = "/v2/variants/13:32889711::A?species=mmusculus&assembly=grcm38";
+        VariantWithSamplesAndAnnotation variantWithSamplesAndAnnotations = restTemplate.exchange(
+                url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<VariantWithSamplesAndAnnotation>() {
+                }).getBody();
+        assertEquals("13", variantWithSamplesAndAnnotations.getChromosome());
+        assertNull(variantWithSamplesAndAnnotations.getReference());
+        assertEquals("A", variantWithSamplesAndAnnotations.getAlternate());
+        assertEquals(32889711, variantWithSamplesAndAnnotations.getStart());
+    }
+
 }
