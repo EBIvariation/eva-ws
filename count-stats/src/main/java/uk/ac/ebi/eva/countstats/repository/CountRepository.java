ad5fb91217a86497543275e6c6b9bf1410256249
nkumar2 <nkumar2@ebi.ac.uk>
2021-07-27 18:33:27 +0100

    few changes added for deployment

--- count-stats/src/main/java/uk/ac/ebi/eva/countstats/repository/CountRepository.java
+++ count-stats/src/main/java/uk/ac/ebi/eva/countstats/repository/CountRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import uk.ac.ebi.eva.countstats.model.Count;
 
 public interface CountRepository extends CrudRepository<Count, Long> {
-    @Query(value = "SELECT sum(pcm.count) FROM process_count_metric pcm WHERE pcm.process=?1 and pcm.identifier->>'study'=?2", nativeQuery = true)
+    @Query(value = "SELECT sum(cs.count) FROM count_stats cs WHERE cs.process=?1 and cs.identifier->>'study'=?2", nativeQuery = true)
     Long getCountForProcess(String process, String study);
 }
