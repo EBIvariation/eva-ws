fd211c0ba061f9761583126b821a40623cdc3daf
nkumar2 <nkumar2@ebi.ac.uk>
2021-07-27 14:27:08 +0100

    Initial Commit

--- count-stats/src/main/java/uk/ac/ebi/eva/countstats/CountStatsApplication.java
+++ count-stats/src/main/java/uk/ac/ebi/eva/countstats/CountStatsApplication.java
@@ -2,12 +2,17 @@ package uk.ac.ebi.eva.countstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountStatsApplication {
+public class CountStatsApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CountStatsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CountStatsApplication.class, args);
     }
-
 }
