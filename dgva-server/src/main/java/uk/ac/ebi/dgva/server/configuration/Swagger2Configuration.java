ff1df00b9e752fd2155d84065c0654c9af86fcc9
tcezard <tcezard@ebi.ac.uk>
2026-05-21 22:54:26 +0100

    Revert change

--- dgva-server/src/main/java/uk/ac/ebi/dgva/server/configuration/Swagger2Configuration.java
+++ dgva-server/src/main/java/uk/ac/ebi/dgva/server/configuration/Swagger2Configuration.java
@@ -24,10 +24,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class Swagger2Configuration {
 
     @Bean
